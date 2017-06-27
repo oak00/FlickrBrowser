eee1f2dfcc564eeda411651b33f450d51d59bd86
Osman Ak <oaak001@gmail.com>
2017-06-26 20:48:40 -0500

    Begun implementation of Searching functionality Completed styles implementation

--- app/src/main/java/com/osmanak/flickrbrowser/PhotoDetailActivity.java
+++ app/src/main/java/com/osmanak/flickrbrowser/PhotoDetailActivity.java
@@ -1,6 +1,7 @@
 package com.osmanak.flickrbrowser;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,10 +23,15 @@ public class PhotoDetailActivity extends BaseActivity {
         Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
         if(photo != null){
             TextView photoTitle = (TextView) findViewById(R.id.photo_title);
-            photoTitle.setText("Title: " + photo.getTitle());
+
+            //Grabs resources from resource files
+            Resources resources = getResources();
+
+            //Substitutes placeholder text with the String resource
+            photoTitle.setText(resources.getString(R.string.photo_title_text, photo.getTitle()));
 
             TextView photoTags = (TextView) findViewById(R.id.photo_tags);
-            photoTags.setText("Tags: " + photo.getTags());
+            photoTags.setText(resources.getString(R.string.photo_tags_text, photo.getTags()));
 
             TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
             photoAuthor.setText(photo.getAuthor());
